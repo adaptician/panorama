452ca3a27a79f29534c6cf5a91a135a4b4d0a592
Tyler Pieterse <admin@adaptica.digital>
2024-09-25 10:02:26 +0200

    Made ABP documentation menu auto-collapse

--- src/Panorama.Web.Host/src/shared/layout/menu-item.ts
+++ src/Panorama.Web.Host/src/shared/layout/menu-item.ts
@@ -14,12 +14,16 @@ export class MenuItem {
     route: string,
     icon: string,
     permissionName: string = null,
-    children: MenuItem[] = null
+    children: MenuItem[] = null,
+    isActive = true,
+    isCollapsed = true
   ) {
     this.label = label;
     this.route = route;
     this.icon = icon;
     this.permissionName = permissionName;
     this.children = children;
+    this.isActive = isActive;
+    this.isCollapsed = isCollapsed;
   }
 }
