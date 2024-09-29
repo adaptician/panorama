8a57ed6ccffbd6c3ff7425ce1152409f236d69ae
Tyler Pieterse <admin@adaptica.digital>
2024-09-29 15:23:55 +0200

    Created abp module for event bus, which will encapsulate the specifics of RabbitMq and MassTransit

---
+++ src/Panorama.Backing.Bus/PanoramaBackingBusModule.cs
@@ -0,0 +1,12 @@
+﻿using Abp.Modules;
+using Abp.Reflection.Extensions;
+
+namespace Panorama.Backing;
+
+public class PanoramaBackingBusModule : AbpModule
+{
+    public override void Initialize()
+    {
+        IocManager.RegisterAssemblyByConvention(typeof(PanoramaBackingBusModule).GetAssembly());
+    }
+}
